e79a453dc2baf54ec215c540769efd24c8987608
Maximilian Misiurski <85294320+MaxTheRock@users.noreply.github.com>
2026-07-30 19:51:19 +0100

    room changes
    - added level 1 computer
    - added hitboxes

--- scripts/room_computer.gd
+++ scripts/room_computer.gd
@@ -1,10 +1,18 @@
 extends Sprite2D
 
 @onready var label: Label = $Label
+@onready var computer_light = $computer_light
 var label_shown: bool = false
 
 func _ready() -> void:
 	label.hide()
+	randomize()
+	run_loop()
+
+func run_loop() -> void:
+	while true:
+		await get_tree().create_timer(randf_range(0.01, 1)).timeout
+		flickering()
 
 func _process(delta: float) -> void:
 	if label_shown == true and Input.is_action_just_pressed("interact"):
@@ -19,3 +27,9 @@ func _on_area_2d_area_exited(area: Area2D) -> void:
 	if area.name == "Player_Detector":
 		label_shown = false
 		label.hide()
+		
+func flickering() -> void:
+	if computer_light.modulate == Color(1, 1, 1, 0.2):
+		computer_light.modulate = Color(1, 1, 1, 0.25)
+	else:
+		computer_light.modulate = Color(1, 1, 1, 0.2)
