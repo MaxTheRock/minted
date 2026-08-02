d2a25a0efcadb427c06e3ef63a63a2b55d9a32f3
Maximilian Misiurski <85294320+MaxTheRock@users.noreply.github.com>
2026-08-02 12:29:14 +0100

    bit more upgrade code
    still not done

--- scenes/upgrades_container.gd
+++ scenes/upgrades_container.gd
@@ -1,18 +1,20 @@
 extends Control
 
 const pricing = {
-	"lv2_bed": 1000
+	"lv2_computer": 2000,
+	"lv2_bed": 1000,
+	"lv2_wardrobe": 500,
+	"lv2_shelf": 1500
 }
 
-@onready var lv2_bed = $PanelContainer/MarginContainer/VBoxContainer/Frame/LV2_bed
-@onready var button = $PanelContainer/MarginContainer/VBoxContainer/Button
-@onready var sprite_container = $PanelContainer/MarginContainer/VBoxContainer/Frame
+@onready var button = $Button/PanelContainer/MarginContainer/VBoxContainer/Button
+@onready var sprite_container = $Button/PanelContainer/MarginContainer/VBoxContainer/Frame
 @export var upgrade = ""
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	for sprite in sprite_container.get_children():
-		if upgrade == sprite.name:
+		if upgrade == sprite.name:                                 
 			sprite.show()
 			button.text = str(pricing.get(sprite.name, 0))
 		else:
